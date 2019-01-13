9bf27681bfe014718ec3f15932a5fea10a470f14
Haewun Cha <hanmircha@naver.com>
2019-01-13 20:15:30 +0900

    홈페이지와 아두이노 연동까지 제작(홈페이지 구성은 안함)

--- FishBerry/app.js
+++ FishBerry/app.js
@@ -8,6 +8,21 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+// 시리얼 포트 설정
+// COMxx : 아두이노가 연결된 xx번 포트
+// 시리얼포트 사용을 위해 npm --save install serialport 설치를 진행해야 한다
+// 설치오류가 나면 파이썬 2버전을 설치해야 하므로 npm install -g windows-build-tools 명령을 실행한다
+var serialPort  = require('serialport');
+var com55 = new serialPort('COM55',{
+  baudRate : 9600,
+  dataBits : 8,
+  parity : 'none',
+  stopBits: 1,
+  flowControl: false
+});
+com55.on('open', function () {
+  console.log('open serial communication');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));  // view에 대한 폴더는 views로 지정
