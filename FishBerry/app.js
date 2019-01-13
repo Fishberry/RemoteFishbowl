2d7f50469d458768a12f0b830db1dd2ad2c57269
Haewun Cha <hanmircha@naver.com>
2019-01-13 17:55:54 +0900

    홈페이지 리뉴얼

--- FishBerry/app.js
+++ FishBerry/app.js
@@ -1,18 +1,20 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// 라우터 모듈 불러오기
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));  // view에 대한 폴더는 views로 지정
+app.set('view engine', 'ejs');  // 템플릿 엔진은 EJS 사용
+app.set('port', process.env.PORT || 8001);
 
+// 미들웨어 구성
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,20 +24,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// 라우터 이후에 404 에러 처리
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// 에러 핸들러
+app.use((err, req, res) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-module.exports = app;
+// 서버를 실행시킨 후 클라이언트 대기
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중...');
+});
--- FishBerry/routes/index.js
+++ FishBerry/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', (req, res, next) => {
   // DB에서 데이터를 찾은 후 보내줄 수도 있다.
   res.render('index', {
-    title: 'Express'
+    title: 'FishBerry'
     // 이곳에 렌더링 할때 동봉할 객체들을 기술한다
   });
 });
