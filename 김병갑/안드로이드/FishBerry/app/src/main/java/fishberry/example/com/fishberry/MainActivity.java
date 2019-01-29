54a67d879d7741bdbd9550620e388ee7a4ed02f8
chahaun <hanmircha@naver.com>
2019-01-29 18:23:56 +0900

    메인에 온도연동

--- 김병갑/안드로이드/FishBerry/app/src/main/java/fishberry/example/com/fishberry/MainActivity.java
+++ 김병갑/안드로이드/FishBerry/app/src/main/java/fishberry/example/com/fishberry/MainActivity.java
@@ -5,42 +5,52 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
+import android.view.*;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.*;
+import io.socket.client.IO;
+import io.socket.client.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
     WebView webView;            //웹뷰 객체
     WebSettings webSettings;    //웹뷰 세팅 객체
-
-    private Button mainButton01;
+    private Socket socket;
+    private TextView tempValue;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tempValue = (TextView) findViewById(R.id.TempValue);
+        btnSubmit = (Button) findViewById(R.id.MainButton04);
+
+        btnSubmit.setOnClickListener((view)->{
+            socket.emit("reqMsg", "메세지");
+        });
+
+        try {
+            socket = IO.socket("http://192.168.0.114:3000");
+            socket.on(Socket.EVENT_CONNECT, (Object... objects) -> {
+            }).on("serverMessage", (Object... objects) -> {
+                runOnUiThread(()->{
+                    tempValue.setText(objects[0].toString());
+                });
+            });
+            socket.connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         //웹뷰 객체 레이아웃 아이디와 매칭 및 설정
         webView = (WebView) findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
         webSettings = webView.getSettings();    //웹뷰의 세팅을 웹뷰세팅 객체에게 받는다.
         webSettings.setJavaScriptEnabled(true);     //자바스크립트 사용 가능
-
-//        mainButton01 = (Button) findViewById(R.id.MainButton01);
-//        mainButton01.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, TemperActivity.class);
-//                startActivity(intent);
-//            }
-//        });
 
         //원하는 URL 됨.
         //webView.loadUrl("URL");
