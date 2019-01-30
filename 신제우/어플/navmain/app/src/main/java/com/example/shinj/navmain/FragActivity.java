f10f7a4563a118deb6a4a183ec1415564f8b1cf1
shin-je-woo <39439576+shin-je-woo@users.noreply.github.com>
2019-01-30 20:54:22 +0900

    nav bar 완료
    완료! 빠진거 있으면 말씀해주세요~

--- 신제우/어플/navmain/app/src/main/java/com/example/shinj/navmain/FragActivity.java
+++ 신제우/어플/navmain/app/src/main/java/com/example/shinj/navmain/FragActivity.java
@@ -2,9 +2,19 @@ package com.example.shinj.navmain;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,6 +24,10 @@ import io.socket.client.Socket;
 
 
 public class FragActivity extends AppCompatActivity implements View.OnClickListener{
+    public DrawerLayout mDrawerLayout;
+    public Toolbar toolbar;
+    public ActionBar actionBar;
+
     Button bt1,bt2;
     FragmentManager fm;
     FragmentTransaction tran;
@@ -33,6 +47,58 @@ public class FragActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frag);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            /* 네비게이션바의 메뉴를 클릭했을 때 동작을 지정하는 메소드임! 질문은 제우에게 */
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                mDrawerLayout.closeDrawers();
+                Intent intent;
+                int id = menuItem.getItemId();
+                switch (id) {
+                    //  네비게이션바 - 스트리밍
+                    case R.id.navigation_item_streaming:
+                        intent = new Intent(FragActivity.this, StreamingActivity.class);
+                        startActivity(intent);
+                        Toast.makeText(FragActivity.this, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                        break;
+
+                    // 네비게이션바 - 설정
+                    case R.id.navigation_item_settings:
+                        intent = new Intent(FragActivity.this, FragActivity.class);
+                        startActivity(intent);
+                        Toast.makeText(FragActivity.this, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                        break;
+
+                    // 네비게이션바 - 알람
+                    case R.id.navigation_item_alarm:
+                        Toast.makeText(FragActivity.this, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                        break;
+
+                    case R.id.nav_sub_menu_item01:
+                        Toast.makeText(FragActivity.this, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                        break;
+
+                    case R.id.nav_sub_menu_item02:
+                        Toast.makeText(FragActivity.this, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                        break;
+
+                }
+
+                return true;
+            }
+        });
+
         bt1 = (Button) findViewById(R.id.bt1);
         bt2 = (Button) findViewById(R.id.bt2);
         bt1.setOnClickListener(this);
@@ -194,6 +260,45 @@ public class FragActivity extends AppCompatActivity implements View.OnClickListe
 //    public void cancelTemperButton(View v) {
 //        finish();
 //    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        Intent intent;
+
+        switch (id) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.action_settings:
+                return true;
+
+            /* 우측 상단위 옵션메뉴 */
+            case R.id.usingGuide:
+                intent = new Intent(this, ManualActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.userSetting:
+                intent = new Intent(this, SettingActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.Inquire:
+                Toast.makeText(this, "문의 : https://github.com/Fishberry/RemoteFishbowl", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public void onBackPressed() {
--- 신제우/어플/navmain/app/src/main/java/com/example/shinj/navmain/StreamingActivity.java
+++ 신제우/어플/navmain/app/src/main/java/com/example/shinj/navmain/StreamingActivity.java
@@ -2,9 +2,14 @@ package com.example.shinj.navmain;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -20,6 +25,9 @@ import io.socket.client.IO;
 import io.socket.client.Socket;
 
 public class StreamingActivity extends AppCompatActivity {
+    public DrawerLayout mDrawerLayout;
+    public Toolbar toolbar;
+    public ActionBar actionBar;
 
     WebView webView;            //웹뷰 객체
     WebSettings webSettings;    //웹뷰 세팅 객체
@@ -30,6 +38,57 @@ public class StreamingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_streaming);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            /* 네비게이션바의 메뉴를 클릭했을 때 동작을 지정하는 메소드임! 질문은 제우에게 */
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                mDrawerLayout.closeDrawers();
+                Intent intent;
+                int id = menuItem.getItemId();
+                switch (id) {
+                    //  네비게이션바 - 스트리밍
+                    case R.id.navigation_item_streaming:
+                        intent = new Intent(StreamingActivity.this, StreamingActivity.class);
+                        startActivity(intent);
+                        Toast.makeText(StreamingActivity.this, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                        break;
+
+                    // 네비게이션바 - 설정
+                    case R.id.navigation_item_settings:
+                        intent = new Intent(StreamingActivity.this, FragActivity.class);
+                        startActivity(intent);
+                        Toast.makeText(StreamingActivity.this, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                        break;
+
+                    // 네비게이션바 - 알람
+                    case R.id.navigation_item_alarm:
+                        Toast.makeText(StreamingActivity.this, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                        break;
+
+                    case R.id.nav_sub_menu_item01:
+                        Toast.makeText(StreamingActivity.this, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                        break;
+
+                    case R.id.nav_sub_menu_item02:
+                        Toast.makeText(StreamingActivity.this, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                        break;
+
+                }
+
+                return true;
+            }
+        });
 
         tempValue = (TextView) findViewById(R.id.TempValue);
 
@@ -72,6 +131,45 @@ public class StreamingActivity extends AppCompatActivity {
         //원하는 URL 됨.
         webView.loadUrl("http://175.204.79.66:8080/?action=stream");
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        Intent intent;
+
+        switch (id) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.action_settings:
+                return true;
+
+            /* 우측 상단위 옵션메뉴 */
+            case R.id.usingGuide:
+                intent = new Intent(this, ManualActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.userSetting:
+                intent = new Intent(this, SettingActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.Inquire:
+                Toast.makeText(this, "문의 : https://github.com/Fishberry/RemoteFishbowl", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
