b8ebeb52ffeebd33619fe9a0a8f9ec7d111e486a
KimByungGab <40555156+KimByungGab@users.noreply.github.com>
2019-03-18 18:43:43 +0900

    Notification 데몬 완성
    Notification 데몬 완성

--- 0201_메인서버_및_APP/app/navmain/app/src/main/java/com/example/shinj/navmain/LoginActivity.java
+++ 0201_메인서버_및_APP/app/navmain/app/src/main/java/com/example/shinj/navmain/LoginActivity.java
@@ -38,6 +38,8 @@ public class LoginActivity extends AppCompatActivity implements Serializable {
     final static String folderName = Environment.getExternalStorageDirectory().getAbsolutePath()+"/connectInfor";
     final static String fileName = "infor.txt";
     final DBHelper dbHelper = new DBHelper(this);
+    private Intent notificationServiceIntent;
+    private NotificationService notificationService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,8 +75,11 @@ public class LoginActivity extends AppCompatActivity implements Serializable {
                     else
                         dbHelper.update(ipEdit.getText().toString());
 
-                    Intent serviceIntent = new Intent(this, NotificationService.class);
-                    startService(serviceIntent);
+                    notificationService = new NotificationService(getApplicationContext());
+                    notificationServiceIntent = new Intent(getApplicationContext(), notificationService.getClass());
+
+                    if (!BootReceiver.isServiceRunning(this, notificationService.getClass()))
+                        startService(notificationServiceIntent);
 
                     intentData.setAddress(ipEdit.getText().toString());
                     intentData.setSocket(socket);
