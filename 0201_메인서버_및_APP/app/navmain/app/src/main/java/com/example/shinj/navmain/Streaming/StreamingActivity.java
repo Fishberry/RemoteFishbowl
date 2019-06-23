d7c51234e5b3e70356fac62d7474a2b1eb83382e
KimByungGab <40555156+KimByungGab@users.noreply.github.com>
2019-06-23 09:31:37 +0900

    갑자기 꺼지는 오류 해결
    갑자기 꺼지는 오류 해결

--- 0201_메인서버_및_APP/app/navmain/app/src/main/java/com/example/shinj/navmain/Streaming/StreamingActivity.java
+++ 0201_메인서버_및_APP/app/navmain/app/src/main/java/com/example/shinj/navmain/Streaming/StreamingActivity.java
@@ -18,6 +18,7 @@ import com.example.shinj.navmain.IntentData;
 import com.example.shinj.navmain.R;
 
 import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 public class StreamingActivity extends BaseActivity implements StreamingPresenter.View {
 
@@ -34,6 +35,7 @@ public class StreamingActivity extends BaseActivity implements StreamingPresente
     boolean isConnectSensor;
     public static String minTempValue, maxTempValue, minPHValue, maxPHValue;
     StreamingPresenterImpl streamingPresenterimpl;
+    ProgressBar progressBarTemperature;
 
     private Handler handler = new Handler();
 
@@ -48,7 +50,8 @@ public class StreamingActivity extends BaseActivity implements StreamingPresente
         phValue = (TextView) findViewById(R.id.pHValue);
         address = intentData.getAddress();
         socket = intentData.getSocket();
-        final ProgressBar progressBarTemperature = (ProgressBar) findViewById(R.id.progresBarTemper);
+//        final ProgressBar progressBarTemperature = (ProgressBar) findViewById(R.id.progresBarTemper);
+        progressBarTemperature = (ProgressBar) findViewById(R.id.progresBarTemper);
         streamingPresenterimpl = new StreamingPresenterImpl();
 
         /* 서버DB에서 먹이급여예약시간을 받아와서 1초에 한번씩 출력하는 쓰레드 */
@@ -59,18 +62,21 @@ public class StreamingActivity extends BaseActivity implements StreamingPresente
                 while (true) {
                     try {
                         streamingPresenterimpl.reqTimerFeed(socket);
-                        socket.on(Socket.EVENT_CONNECT, (Object... objects) -> {
-                        }).on("resTimerFeed", (Object... objects) -> {
-                            runOnUiThread(()-> {
-                                if (!(objects[0].toString().equals("0")) || !(objects[1].toString().equals("0"))) {
-                                    feedTimeRemain = Integer.parseInt(objects[1].toString());
-                                    feedTimer.setText(feedTimeRemain/60/60 + "시간 "
-                                            + feedTimeRemain/60%60 + "분 " +
-                                            + feedTimeRemain%60 + "초 "
-                                            + " 회전수: " + objects[0].toString() );
-                                }
-                            });
-                        });
+                        socket.on(Socket.EVENT_CONNECT, onConnect);
+                        socket.on("resTimerFeed", resTimerFeed);
+
+//                        socket.on(Socket.EVENT_CONNECT, (Object... objects) -> {
+//                        }).on("resTimerFeed", (Object... objects) -> {
+//                            runOnUiThread(()-> {
+//                                if (!(objects[0].toString().equals("0")) || !(objects[1].toString().equals("0"))) {
+//                                    feedTimeRemain = Integer.parseInt(objects[1].toString());
+//                                    feedTimer.setText(feedTimeRemain/60/60 + "시간 "
+//                                            + feedTimeRemain/60%60 + "분 " +
+//                                            + feedTimeRemain%60 + "초 "
+//                                            + " 회전수: " + objects[0].toString() );
+//                                }
+//                            });
+//                        });
                         Thread.sleep(1000);
                     } catch (Exception e) {
                         runOnUiThread(new Runnable() {
@@ -92,18 +98,21 @@ public class StreamingActivity extends BaseActivity implements StreamingPresente
                 while (true) {
                     try {
                         streamingPresenterimpl.reqTimerWater(socket);
-                        socket.on(Socket.EVENT_CONNECT, (Object... objects) -> {
-                        }).on("resTimerWater", (Object... objects) -> {
-                            try {
-                                waterTimeRemain = objects[0].toString();
-                                waterTimer.setText(waterTimeRemain.split("/")[0] + "년"
-                                        + waterTimeRemain.split("/")[1] + "월"
-                                        + waterTimeRemain.split("/")[2] + "일"
-                                        + waterTimeRemain.split("/")[3] + "시"
-                                        + waterTimeRemain.split("/")[4] + "분");
-                            } catch (Exception e) {
-                            }
-                        });
+                        socket.on(Socket.EVENT_CONNECT, onConnect);
+                        socket.on("resTimerWater", resTimerWater);
+
+//                        socket.on(Socket.EVENT_CONNECT, (Object... objects) -> {
+//                        }).on("resTimerWater", (Object... objects) -> {
+//                            try {
+//                                waterTimeRemain = objects[0].toString();
+//                                waterTimer.setText(waterTimeRemain.split("/")[0] + "년"
+//                                        + waterTimeRemain.split("/")[1] + "월"
+//                                        + waterTimeRemain.split("/")[2] + "일"
+//                                        + waterTimeRemain.split("/")[3] + "시"
+//                                        + waterTimeRemain.split("/")[4] + "분");
+//                            } catch (Exception e) {
+//                            }
+//                        });
                         Thread.sleep(1000);
                     } catch (Exception e) {
                         runOnUiThread(new Runnable() {
@@ -126,40 +135,43 @@ public class StreamingActivity extends BaseActivity implements StreamingPresente
                 while (true) {
                     try {
                         streamingPresenterimpl.reqTemperPHMsg(socket);
-                        socket.on(Socket.EVENT_CONNECT, (Object... objects) -> {
-                        }).on("serverMsg", (Object... objects) -> {
-                            minTempValue = objects[2].toString();
-                            maxTempValue = objects[3].toString();
-                            minPHValue = objects[4].toString();
-                            maxPHValue = objects[5].toString();
-                            runOnUiThread(()-> {
-                                if (!(objects[0].toString().equals("0")) || !(objects[1].toString().equals("0"))) {
-                                    setWhetherSensor(true);
-                                    /* 서버DB에서 온도,pH 위험설정값을 받아와서 출력 */
-                                    settingTemperPh.setText(objects[2].toString() + "℃ ~ " + objects[3].toString() + "℃\n"
-                                            + objects[4].toString() + " ~ " + objects[5].toString());
-                                    //
-                                    tempValue.setText(progressTemp = objects[0].toString());
-                                    phValue.setText(objects[1].toString());
-                                    progressTemp = progressTemp.substring(0, 2);    // 온도계 프로그래스바에 온도표시하려고 할 때 서버에서 objects[0]로 받아오는 온도값이 24.xxx처럼 돼 있어서 int형 부분만 자름
-                                    // handler.post는 온도계 프로그래스바에 온도값 표시해주는 메소드
-                                    handler.post(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            if (-40 < Integer.parseInt(progressTemp))
-                                                progressBarTemperature.setProgress(Integer.parseInt(progressTemp));
-                                        }
-                                    });
-                                }
-                                else {
-                                    runOnUiThread(new Runnable() {
-                                        public void run() {
-                                            Toast.makeText(StreamingActivity.this, "온도센서 혹은 수질센서에서 값을 받아 올 수 없습니다.", Toast.LENGTH_SHORT).show();
-                                        }
-                                    });
-                                }
-                            });
-                        });
+                        socket.on(Socket.EVENT_CONNECT, onConnect);
+                        socket.on("serverMsg", serverMsg);
+
+//                        socket.on(Socket.EVENT_CONNECT, (Object... objects) -> {
+//                        }).on("serverMsg", (Object... objects) -> {
+//                            minTempValue = objects[2].toString();
+//                            maxTempValue = objects[3].toString();
+//                            minPHValue = objects[4].toString();
+//                            maxPHValue = objects[5].toString();
+//                            runOnUiThread(()-> {
+//                                if (!(objects[0].toString().equals("0")) || !(objects[1].toString().equals("0"))) {
+//                                    setWhetherSensor(true);
+//                                    /* 서버DB에서 온도,pH 위험설정값을 받아와서 출력 */
+//                                    settingTemperPh.setText(objects[2].toString() + "℃ ~ " + objects[3].toString() + "℃\n"
+//                                            + objects[4].toString() + " ~ " + objects[5].toString());
+//                                    //
+//                                    tempValue.setText(progressTemp = objects[0].toString());
+//                                    phValue.setText(objects[1].toString());
+//                                    progressTemp = progressTemp.substring(0, 2);    // 온도계 프로그래스바에 온도표시하려고 할 때 서버에서 objects[0]로 받아오는 온도값이 24.xxx처럼 돼 있어서 int형 부분만 자름
+//                                    // handler.post는 온도계 프로그래스바에 온도값 표시해주는 메소드
+//                                    handler.post(new Runnable() {
+//                                        @Override
+//                                        public void run() {
+//                                            if (-40 < Integer.parseInt(progressTemp))
+//                                                progressBarTemperature.setProgress(Integer.parseInt(progressTemp));
+//                                        }
+//                                    });
+//                                }
+//                                else {
+//                                    runOnUiThread(new Runnable() {
+//                                        public void run() {
+//                                            Toast.makeText(StreamingActivity.this, "온도센서 혹은 수질센서에서 값을 받아 올 수 없습니다.", Toast.LENGTH_SHORT).show();
+//                                        }
+//                                    });
+//                                }
+//                            });
+//                        });
                         Thread.sleep(1000);
                     } catch (Exception e) {
                         runOnUiThread(new Runnable() {
@@ -248,4 +260,87 @@ public class StreamingActivity extends BaseActivity implements StreamingPresente
     protected int getLayoutResource() {
         return R.layout.activity_streaming;
     }
+
+    private Emitter.Listener onConnect = new Emitter.Listener() {
+        @Override
+        public void call(Object... objects) {
+
+        }
+    };
+
+    private Emitter.Listener resTimerFeed = new Emitter.Listener() {
+        @Override
+        public void call(Object... objects) {
+            runOnUiThread(()-> {
+                if (!(objects[0].toString().equals("0")) || !(objects[1].toString().equals("0"))) {
+                    feedTimeRemain = Integer.parseInt(objects[1].toString());
+                    feedTimer.setText(feedTimeRemain/60/60 + "시간 "
+                            + feedTimeRemain/60%60 + "분 " +
+                            + feedTimeRemain%60 + "초 "
+                            + " 회전수: " + objects[0].toString() );
+                }
+            });
+        }
+    };
+
+    private Emitter.Listener resTimerWater = new Emitter.Listener() {
+        @Override
+        public void call(Object... objects) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        waterTimeRemain = objects[0].toString();
+                        waterTimer.setText(waterTimeRemain.split("/")[0] + "년"
+                                + waterTimeRemain.split("/")[1] + "월"
+                                + waterTimeRemain.split("/")[2] + "일"
+                                + waterTimeRemain.split("/")[3] + "시"
+                                + waterTimeRemain.split("/")[4] + "분");
+                    } catch (Exception e) {
+                    }
+                }
+            });
+        }
+    };
+
+    private Emitter.Listener serverMsg = new Emitter.Listener() {
+        @Override
+        public void call(Object... objects) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    minTempValue = objects[2].toString();
+                    maxTempValue = objects[3].toString();
+                    minPHValue = objects[4].toString();
+                    maxPHValue = objects[5].toString();
+
+                    if (!(objects[0].toString().equals("0")) || !(objects[1].toString().equals("0"))) {
+                        setWhetherSensor(true);
+                        /* 서버DB에서 온도,pH 위험설정값을 받아와서 출력 */
+                        settingTemperPh.setText(objects[2].toString() + "℃ ~ " + objects[3].toString() + "℃\n"
+                                + objects[4].toString() + " ~ " + objects[5].toString());
+                        //
+                        tempValue.setText(progressTemp = objects[0].toString());
+                        phValue.setText(objects[1].toString());
+                        progressTemp = progressTemp.substring(0, 2);    // 온도계 프로그래스바에 온도표시하려고 할 때 서버에서 objects[0]로 받아오는 온도값이 24.xxx처럼 돼 있어서 int형 부분만 자름
+                        // handler.post는 온도계 프로그래스바에 온도값 표시해주는 메소드
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (-40 < Integer.parseInt(progressTemp))
+                                    progressBarTemperature.setProgress(Integer.parseInt(progressTemp));
+                            }
+                        });
+                    }
+                    else {
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                Toast.makeText(StreamingActivity.this, "온도센서 혹은 수질센서에서 값을 받아 올 수 없습니다.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    };
 }
